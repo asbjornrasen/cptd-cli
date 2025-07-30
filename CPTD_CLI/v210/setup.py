6d66ac9074696109d9b7e70526aff085381aa9e5
Asbjorn <asbjornrasen@gmail.com>
2025-07-30 21:37:21 +0200

    add v210

--- CPTD_CLI/v210/setup.py
+++ CPTD_CLI/v210/setup.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 setup(
     name='cptd',
-    version='2.0.9',
+    version='2.1.0',
     description='CPTD CLI',
     author='Asbjorn Rasen',
     author_email='asbjornrasen@gmail.com',
